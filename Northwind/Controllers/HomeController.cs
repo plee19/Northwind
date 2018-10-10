335b9a71dbfd883014723f879fb09e134f57a9a3
plee19 <plee19@my.wctc.edu>
2018-10-10 15:48:01 -0500

    PrimaryColors works.

--- Northwind/Controllers/HomeController.cs
+++ Northwind/Controllers/HomeController.cs
@@ -45,8 +45,19 @@ namespace Northwind.Controllers
             return View();
         }
 
-        public ActionResult PrimaryColors()
+        public ActionResult PrimaryColors(FormCollection form)
         {
+            ViewBag.Color1 = form["color1"];
+            ViewBag.Color2 = form["color2"];
+            if (ViewBag.Color1 == "red" && ViewBag.Color2 == "red") { ViewBag.Color3 = "red"; }
+            if (ViewBag.Color1 == "blue" && ViewBag.Color2 == "blue") { ViewBag.Color3 = "blue"; }
+            if (ViewBag.Color1 == "green" && ViewBag.Color2 == "green") { ViewBag.Color3 = "green"; }
+            if ((ViewBag.Color1 == "red" && ViewBag.Color2 == "blue") ||
+               (ViewBag.Color1 == "blue" && ViewBag.Color2 == "red")) { ViewBag.Color3 = "magenta"; }
+            if ((ViewBag.Color1 == "red" && ViewBag.Color2 == "green") ||
+               (ViewBag.Color1 == "green" && ViewBag.Color2 == "red")) { ViewBag.Color3 = "yellow"; }
+            if ((ViewBag.Color1 == "green" && ViewBag.Color2 == "blue") ||
+               (ViewBag.Color1 == "blue" && ViewBag.Color2 == "green")) { ViewBag.Color3 = "cyan"; }
             return View();
         }
 
