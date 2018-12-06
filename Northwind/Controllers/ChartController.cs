923e5fe025596014378119e009f803a69d995e2a
plee19 <42356593+plee19@users.noreply.github.com>
2018-12-06 13:59:13 -0600

    ChartController and first index page created as category list

---
+++ Northwind/Controllers/ChartController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Northwind.Controllers
+{
+    public class ChartController : Controller
+    {
+        // GET: Chart
+        public ActionResult Index()
+        {
+            using (NorthwindEntities db = new NorthwindEntities())
+            {
+                return View(db.Categories.OrderBy(c => c.CategoryName).ToList());
+            }
+        }
+
+        // GET: Chart/1
+        /*public JsonResult Chart(int id)
+        {
+
+        }*/
+    }
+}
