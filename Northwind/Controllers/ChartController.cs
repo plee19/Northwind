abf4409a817045ff61f07d2bff81c7ffc17d8a56
plee19 <plee19@my.wctc.edu>
2018-12-11 21:21:11 -0600

    Working Category sales query

--- Northwind/Controllers/ChartController.cs
+++ Northwind/Controllers/ChartController.cs
@@ -17,6 +17,23 @@ namespace Northwind.Controllers
             }
         }
 
+        public JsonResult FilterChart(int? categoryID)
+        {
+            using (NorthwindEntities db = new NorthwindEntities())
+            {
+                var categoryInfo =
+                    from p in db.Products
+                    join c in db.Categories on p.CategoryID equals c.CategoryID
+                    join od in db.Order_Details on p.ProductID equals od.ProductID
+                    group new { p, c, od } by new { c.CategoryName } into f
+                    select new { CategoryName = f.Key.CategoryName, Sum = (decimal?)f.Sum(s => s.od.Quantity * s.od.UnitPrice * (1 - s.od.Discount)) };  //Sum = (od.UnitPrice * od.Quantity * (1 - od.Discount)) };
+
+                var catInfo = categoryInfo.ToList();
+
+                return Json(catInfo, JsonRequestBehavior.AllowGet);
+            } 
+        }
+
         // GET: Chart/1
         /*public JsonResult Chart(int id)
         {
